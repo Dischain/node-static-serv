a53185f717c7f53bc3d64989d95a7b3e0f2b966e
Aleksandr Shaytanov <rdischain@yandex.ru>
2017-08-18 22:16:02 +0300

    add winston logging

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const Server = require('./lib/server.js');
 
 module.exports = Server;
 new Server({
+  root: '../',
   cache: true, 
   encoding: true,
   cors: true, 
-  name: 'dischain-serv'}).start();
+  name: 'dischain-serv',
+  notFound: './404.html',
+  logToConsole: true,
+  logToFile: true,
+  logFile: './syslog.log'
+}).start();
