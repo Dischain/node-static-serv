36d5f6c5ea89d8c4447d6a3a0472ba2a3813be22
Aleksandr Shaytanov <rdischain@yandex.ru>
2017-08-25 21:20:17 +0300

    add deafault path to log, cleared stuff

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-const Server = require('./lib/server.js');
-
-module.exports = Server;
-new Server({
-  root: '../',
-  cache: true, 
-  encoding: true,
-  cors: true, 
-  name: 'dischain-serv',
-  notFound: './404.html',
-  logToConsole: true,
-  logToFile: true,
-  logFile: './syslog.log'
-}).start();
+module.exports = require('./lib/server.js');
